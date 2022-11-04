35f87f59418d31a624322dbe3b5c4b160570ab49
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2022-11-04 09:15:57 +0100

    init

---
+++ src/components/long_logo/long_logo.ts
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({
+  name: "long_logo",
+  components: {},
+})
+export default class long_logo extends Vue {}
---
+++ src/components/long_logo/long_logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="long_logo"></div>
+</template>
+
+<script lang="ts" src="./long_logo.ts" />
+<style src="./long_logo.css" />
