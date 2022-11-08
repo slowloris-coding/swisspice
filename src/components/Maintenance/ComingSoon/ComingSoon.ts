b9ac11c8119ec5c0b08def9fa5d9d8b0f9f253d4
slowloris-coding <65670619+slowloris-coding@users.noreply.github.com>
2022-11-08 15:49:34 +0100

    init

---
+++ src/components/Maintenance/ComingSoon/ComingSoon.ts
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({
+  name: "CommingSoon",
+  components: {},
+})
+export default class ComingSoon extends Vue {}
---
+++ src/components/Maintenance/ComingSoon/ComingSoon.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container id="ComingSoon"> </v-container>
+</template>
+
+<script lang="ts" src="ComingSoon.ts" />
+<style src="ComingSoon.css" />
